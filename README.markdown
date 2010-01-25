af1d3d485b9da93fdc4652e644fd22f611b19673
Robert Sosinski <email@robertsosinski.com>
2010-01-24 23:43:35 -0500

    making public

---
+++ README.markdown
@@ -0,0 +1,44 @@
+JS-Cookies Plugin
+=================
+
+JS-Cookies is a simple interface to list, get, set and delete cookies.  If you 
+are also using jQuery and jQuery-JSON, you can set entire object literals as 
+cookies too, as they will be automatically converted to and evaluated as JSON.
+
+If you are using a different JavaScript framework or JSON library, you can also 
+easily edit `getJSON` and `setJSON` to use the appropriate methods.
+
+Usage
+=====
+
+Within the scope of the current domain, you can list all cookies:
+
+    cookies.list(); // => ["apple", "banana", "cherry"]
+
+Show the value within a cookie:
+
+    cookies.get("apple"); // => "macintosh"
+
+Set the value for a cookie:
+
+    cookies.set("banana", "cavendish"); // => undefined
+
+And also delete a cookie.
+
+    cookies.delete_("banana"); // => undefined
+
+NOTE: JS-Cookies uses "delete_" with a trailing underscore as "delete" is a 
+reserved word in JavaScript, and its usage fails on certain browsers.
+
+You can also get and set object literals using `getJSON` and `setJSON`.
+  
+    cookies.setJSON("basket", {"pear": "bosc", "kiwi": "little spotted"}); // => undefined
+    
+    cookies.getJSON("basket"); // => {"pear": "bosc", "kiwi": "little spotted"}
+
+Feedback
+========
+
+If you have any questions, comments or just want to talk shop about JavaScript, feel free to reach me 
+through my website at http://www.robertsosinski.com.
+
---
+++ cookies.js
@@ -0,0 +1,80 @@
+/*
+ * JS Cookies Library
+ *
+ * Copyright (c) 2010 Robert Sosinski (http://www.robertsosinski.com)
+ * Offical Web Site (http://github.com/robertsosinski/js-cookies)
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+window.cookies = {
+  set: function(name, value, days) {
+    if (name) {
+      if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+      }
+      else {
+        var expires = "";
+      }
+      document.cookie = name + "=" + escape(value) + expires + "; path=/";
+    }
+  },
+  
+  // requires jQuery and jQuery-JSON
+  setJSON: function(name, value, days) {
+    this.set(name, jQuery.toJSON(value), days);
+  },
+  
+  get: function(name) {
+    if (name) {
+      var match = document.cookie.match(new RegExp(name + "=(.*?)(?:;|$)"));      
+      if (match) {
+        return unescape(match[1].replace(/\+/g, "%20"));
+      }
+    }
+  },
+  
+  // requires jQuery and jQuery-JSON
+  getJSON: function(name) {
+    var value = this.get(name);
+    if (value) {
+      return jQuery.evalJSON(value);
+    }
+  },
+  
+  list: function() {
+    var matches = document.cookie.split(new RegExp("=|; "));
+    if (matches.length > 1) {
+      var cookies = new Array(matches.length / 2);
+      for (var i = j = 0; j < matches.length; j+=2) {
+        cookies[i++] = matches[j]
+      }
+      return cookies;
+    }
+  },
+  
+  // delete is a reserved word, so appending an underscore
+  delete_: function(name) {
+    this.set(name, "" , -1);
+  }
+}
